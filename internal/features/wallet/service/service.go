122a4297f5cb20deb1f37ea6b9095d0190faa275
wizardVadim <vizardhdd@gmail.com>
2026-09-06 03:15:02 +0800

    removed commented code

--- internal/features/wallet/service/service.go
+++ internal/features/wallet/service/service.go
@@ -25,48 +25,6 @@ func (s *Service) CreateNewWallet(ctx context.Context) (domain.Wallet, error) {
 	return s.repo.CreateNewWallet(ctx, walletID)
 }
 
-// deprecated
-// func (s *Service) ChangeWalletBalance(ctx context.Context, operation domain.WalletOperation) error {
-// 	if err := ctx.Err(); err != nil {
-// 		return err
-// 	}
-
-// 	err := s.txManager.WithinTransaction(ctx, func(repo Repository) error {
-// 		balance, err := repo.GetWalletBalanceForUpdate(ctx, operation.WalletID())
-// 		if err != nil {
-// 			return err
-// 		}
-
-// 		var newBalance int64
-// 		switch operation.OperationType() {
-// 		case domain.OperationTypeDeposit:
-// 			if operation.Amount() > math.MaxInt64-balance {
-// 				return ErrBalanceOverflow
-// 			}
-// 			newBalance = balance + operation.Amount()
-// 		case domain.OperationTypeWithdraw:
-// 			if balance < operation.Amount() {
-// 				return ErrSmallBalance
-// 			}
-// 			newBalance = balance - operation.Amount()
-// 		default:
-// 			return domain.ErrInvalidOperationType
-// 		}
-
-// 		wallet, err := domain.NewWallet(operation.WalletID(), newBalance)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		if err := repo.UpdateBalance(ctx, wallet); err != nil {
-// 			return err
-// 		}
-
-// 		return nil
-// 	})
-
-// 	return err
-// }
-
 func (s *Service) ChangeWalletBalance(ctx context.Context, operation domain.WalletOperation) error {
 	if err := ctx.Err(); err != nil {
 		return err
